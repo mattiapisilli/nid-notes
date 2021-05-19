ecbb88a0e8825bdba44303cf33ba4dfe2ecf93fd
mattiapisilli <dev.mattiapisilli@gmail.com>
2021-05-19 09:52:55 +0200

    conflict resolved

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,18 +1,11 @@
 class Note {
-  final int id;
-  final String title;
-  final String content;
-
-  Note(this.id, this.title, this.content);
-
-}
   final int? id;
   final String title;
   final String content;
 
   Note(this.title, this.content, {this.id});
 
-  Map<String, dynamic> toMap() {git
+  Map<String, dynamic> toMap() {
     return {
       'id': id,
       'title': title,
